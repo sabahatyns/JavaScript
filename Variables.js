9967f9e1a294e5c9ddce0dfb050b5753976c6f45
Sabahat Younas <sabahatyounas7@gmail.com>
2024-09-01 10:21:28 +0500

    learn js

---
+++ Variables.js
@@ -0,0 +1,13 @@
+const accountId = 5555;
+var accountEmail =  "sabahatyounas7@gmail.com";
+let accountPassword = 55;
+accountCity = "Lahore";
+
+
+//try to change value of above  variables
+//accountId = 6;
+accountEmail =  "syounas7@gmail.com";
+accountPassword = 56;
+accountCity = "Isb"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]) 
