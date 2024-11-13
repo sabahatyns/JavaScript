eca53ce9a5b9f63d5e2cec538c62fd0c2b700089
sabahatyns <sabahatyounas7@gmail.com>
2024-11-13 18:42:59 +0500

    classes

--- Classes in JS.js
+++ Classes in JS.js
@@ -10,5 +10,3 @@ exampleClass.prototype.getBirthYear = function () {
 }
 console.log(Object.keys(exampleClass.prototype));// check methods in prototype of class exampleClass
 console.dir(exampleClass.prototype);// same check methods in porototype with details
-
-
