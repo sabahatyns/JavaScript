f10165f72f43296a8d58b65ac71b3d1840f334cf
sabahatyns <sabahatyounas7@gmail.com>
2024-11-13 19:16:50 +0500

    classes  ad prototype

--- Classes in JS.js
+++ Classes in JS.js
@@ -1,12 +1,32 @@
 // create class
 class exampleClass {
-//add anything here its also like a function
+    //add anything here its also like a function
+    constructor(firstName, lastName, age) { // constructor is built in method 
+        // when we call class only constructor will run other methods below will be added in prototype not in actual class
+        this.firstName = firstName,
+            this.lastName = lastName,
+            this.age = age
+    }
+    // methods below ill be automatically added in prototype of class not in actual class 
+    // we dont need to do it separately by using .prototype method like we need to do in function
+    // it will add automaticaaly thats why we use classs
+    getBirthYear() {
+        return new Date().getFullYear() - this.age
+    }
+    getFullName() {
+        //logic here
+        return this.firstName + " " + this.lastName
+    }
+
 }
-console.log(typeof exampleClass) // it will return function because class in also just like a function
+
+
+const userOne = new exampleClass("Danial", "Younas", 5,)
+const userTwo = new exampleClass("Faizan", "Younas", 6,)
+//console.log(typeof exampleClass) // it will return function because class in also just like a function
 // we cannot call class without using new keyword
-console.log(new exampleClass())
-exampleClass.prototype.getBirthYear = function () {
-    return new Date().getFullYear() - this.age
-}
-console.log(Object.keys(exampleClass.prototype));// check methods in prototype of class exampleClass
-console.dir(exampleClass.prototype);// same check methods in porototype with details
+console.log(userOne.getBirthYear())// this is in prototype
+
+//console.log(Object.keys(exampleClass.prototype));// check methods in prototype of class exampleClass
+//console.dir(exampleClass.prototype);// same check methods in porototype with details
+
