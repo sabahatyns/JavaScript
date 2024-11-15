fd3afecec46fecd5458d1f79ba23075173adb183
sabahatyns <sabahatyounas7@gmail.com>
2024-11-15 19:42:40 +0500

    setter

--- Getter-and-Setter.js
+++ Getter-and-Setter.js
@@ -1,9 +1,14 @@
-const user ={
-    firstName :'Sabahat',
-    lastName : "Younas",
-    get getFullName () { // getter // we can access it as property by using get
+const user = {
+    //firstName: 'Sabahat',
+    //lastName: "Younas",
+    get getFullName() { // getter // we can access this function as property by using get
         return `${this.firstName}  ${this.lastName}`
+    },
+    set fullName(value) { // Setter /// we can set first and lastnemw automatically with setter we dont need to add separately
+        const [firstName, lastName] = value.split(' ');
+        this.firstName = firstName,
+        this.lastName = lastName
     }
 }
 
-console.log(user.getFullName) 
+console.log(user.fullName = "SABAHAT YOUNAS" , user) 
