ab6e34efb3c52e4e033b86e69bf6ecfbdabca76a
sabahatyns <sabahatyounas7@gmail.com>
2024-11-17 20:25:18 +0500

    numbers

---
+++ 06-Nums-and-Maths.js
@@ -0,0 +1,10 @@
+const Num = 55; // js will treat it as nuber automatically
+console.log(Num)
+
+const number = new Number(666.6666666666)// we can specify too
+console.log(number)
+
+// methods in number
+console.log(number.toString()) // it will convert it to string
+console.log(number.toFixed(3)) // decimal k bad kitny dijit aye gy pass that value
+console.log(number.toPrecision(3)) // kitny  total digit show krwany including after point and before point
