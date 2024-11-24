2a6b04996f43540cb76b17c8cd8a8ec041f29b93
sabahatyns <sabahatyounas7@gmail.com>
2024-11-24 20:24:57 +0500

    scope

--- 03_Basics/02_Scope.js
+++ 03_Basics/02_Scope.js
@@ -9,6 +9,20 @@ if (true) {
 }
 
 //console.log(a, b, c); // only var will be printed accesible but that should not be, that is why we dont use var anymore
-console.log("global scope value", d); 
+console.log("global scope value", d);
 
 
+//in nested functions child function can access variables from parent function but parent function can not access variables that are declared in child variable
+function one() {
+    const usrname = "Me"
+    function two() {
+        console.log(usrname);//it can access username bcz it is a child function and it can access variables that are declared in parent functions
+        const website = "this is the variable in child function"
+    }
+    //console.log(website);// it will give error as it cant access variable from child function
+    two()
+
+}
+one()
+
+//same scope logic will be applied in if else case
