1c9eb172d854130034f6c50a854a1a89f427a7e7
sabahatyns <sabahatyounas7@gmail.com>
2024-11-28 21:34:11 +0500

    loops

---
+++ 05_iterations.js/02_do_while.js
@@ -0,0 +1,26 @@
+//while loop
+
+let index = 0;
+
+while (index <= 66) {
+    //console.log("value of index", index);
+    index = index + 6
+}
+
+
+let array = ['kendall', "kylie", "kim"]
+let arr = 0;
+while (arr < array.length) {
+    console.log(array[arr]);
+    arr = arr + 1
+}
+
+
+//do while loop
+// in this it will print first then it will check condition
+let score = 1
+
+do {
+    console.log(" do while", score);
+    score++
+} while (score <= 11);
