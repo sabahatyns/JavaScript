3536dbc8775c676cb68d52adb12b4a658e8d2891
sabahatyns <sabahatyounas7@gmail.com>
2024-11-29 20:01:54 +0500

    map

--- 05_iterations.js/07_map.js
+++ 05_iterations.js/07_map.js
@@ -1,3 +1,13 @@
 //map method
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const methods = myNums.map((item) => item + 5)
+//console.log(methods);
+
+//chaining
+
+const chaining = myNums.map((item) => item + 55).map((num) => num / 5).filter((item) => item > 5)
+
+
+console.log(chaining);
+
