b1eee443cdb9b2c356fa915baeba3d2cbdc684d3
sabahatyns <sabahatyounas7@gmail.com>
2025-05-26 10:28:44 +0500

    pattern 6

--- PatternWithPython.py
+++ PatternWithPython.py
@@ -25,4 +25,24 @@ def third_patt():
          print(j, end=" ")
         print()
 
-third_patt()
+# third_patt()
+
+
+# forth pattern 
+def forth_patt():
+    for i in range(1, 6):
+        for j in range(1, i+1):
+         print(i, end=" ")
+        print()
+
+# forth_patt()
+
+
+# fifth pattern
+def fifth_patt(n):
+    for i in range(1, n):
+        for j in range(1, n-i+1):
+         print("*", end=" ")
+        print()
+
+fifth_patt(6)
